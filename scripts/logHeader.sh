4718f2bd336cb23e29eaf56331beff204aea19c9
Daniel Cooke <danielcooke1996@gmail.com>
2021-10-01 12:35:23 +0100

    chore: removed unused deploy script in root
    - There was still a deploy script in the root package.json that pointed
      to a non existing file. This was causing the CD to finish with red
      status even though it suceeded.
    
    chore: don't run deploy for chore commits

--- scripts/logHeader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-WHITE=$(echo -en '\033[01;37m')
-YELLOW=$(echo -en '\033[00;33m')
-RESTORE=$(echo -en '\033[0m')
-echo "${YELLOW}========================================"
-echo "${WHITE} $1"
-echo "${YELLOW}========================================${RESTORE}"
