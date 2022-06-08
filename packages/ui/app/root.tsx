e3473c937dbf5e9900504340ea33cffb495a3e35
Daniel Cooke <danielcooke1996@gmail.com>
2022-06-08 14:59:45 +0100

    Add 600 weight font

--- packages/ui/app/root.tsx
+++ packages/ui/app/root.tsx
@@ -64,7 +64,7 @@ const Document = withEmotionCache(
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
           {title ? <title>{title}</title> : null}
