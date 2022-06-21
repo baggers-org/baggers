6baafc9f75ba2c8a09e2526fbf64b56e29955d3a
Daniel Cooke <danielcooke1996@gmail.com>
2022-06-21 11:26:01 +0200

    Fix env for CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'cypress';
 
 import dotenv from 'dotenv';
 
-if (process.env.CYPRESS_BASE_URL === 'https://baggers-staging.fly.dev') {
-  dotenv.config({ path: './packages/ui/.env.staging' });
-  dotenv.config({ path: './packages/api/.env.staging' });
-} else {
+if (!process.env.CI) {
   dotenv.config({ path: './packages/api/.env' });
   dotenv.config({ path: './packages/ui/.env' });
 }
@@ -13,12 +10,12 @@ if (process.env.CYPRESS_BASE_URL === 'https://baggers-staging.fly.dev') {
 const {
   CYPRESS_AUTH0_USERNAME,
   CYPRESS_AUTH0_PASSWORD,
-  AUTH0_DOMAIN,
-  AUTH0_CLIENT_ID,
-  AUTH0_CLIENT_SECRET,
-  AUTH0_CALLBACK_URL,
-  AUTH0_AUDIENCE,
-  AUTH0_SCOPE,
+  CYPRESS_AUTH0_DOMAIN,
+  CYPRESS_AUTH0_CLIENT_ID,
+  CYPRESS_AUTH0_CLIENT_SECRET,
+  CYPRESS_AUTH0_CALLBACK_URL,
+  CYPRESS_AUTH0_AUDIENCE,
+  CYPRESS_AUTH0_SCOPE,
 } = process.env;
 
 if (!process.env.ATLAS_CLUSTER_URI)
@@ -30,12 +27,12 @@ export default defineConfig({
     env: {
       Auth0Username: CYPRESS_AUTH0_USERNAME,
       Auth0Password: CYPRESS_AUTH0_PASSWORD,
-      Auth0Domain: AUTH0_DOMAIN,
-      Auth0ClientId: AUTH0_CLIENT_ID,
-      Auth0ClientSecret: AUTH0_CLIENT_SECRET,
-      Auth0CallbackUrl: AUTH0_CALLBACK_URL,
-      Auth0Audience: AUTH0_AUDIENCE,
-      Auth0Scope: AUTH0_SCOPE,
+      Auth0Domain: CYPRESS_AUTH0_DOMAIN,
+      Auth0ClientId: CYPRESS_AUTH0_CLIENT_ID,
+      Auth0ClientSecret: CYPRESS_AUTH0_CLIENT_SECRET,
+      Auth0CallbackUrl: CYPRESS_AUTH0_CALLBACK_URL,
+      Auth0Audience: CYPRESS_AUTH0_AUDIENCE,
+      Auth0Scope: CYPRESS_AUTH0_SCOPE,
     },
   },
 });
