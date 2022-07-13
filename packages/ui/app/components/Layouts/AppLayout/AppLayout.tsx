44be1bdef086083b18b37edc2c3507f68029cea1
Daniel Cooke <danielcooke1996@gmail.com>
2022-07-13 16:38:16 +0200

    Fix stupid yarn.lock dependency hell shit
    * Oh my god, this took me so long
    
    * Note to future self, never ever ever continue working on a project
      until you are 100% certain your CI is using the exact modules you
      expect it to be using. An "immutable" lockfile is one of the first
      requirements for a deployment pipeline. I have lost an entire day and
      stressed so much trying to fix this issue

--- packages/ui/app/components/Layouts/AppLayout/AppLayout.tsx
+++ packages/ui/app/components/Layouts/AppLayout/AppLayout.tsx
@@ -1,13 +1,21 @@
 import { Box } from '@mui/system';
+import { useLocation } from '@remix-run/react';
+import React from 'react';
+
+import { AppBar } from '~/components/AppBar';
 import { Footer } from '~/components/Footer';
+import { useIsMobile } from '~/hooks';
 
 export const AppLayout: React.FC = ({ children }) => {
+  const { pathname } = useLocation();
+  const isMobile = useIsMobile();
+
   return (
     <>
-      {/* <AppBar /> */}
+      <AppBar />
       <Box
         sx={{
-          mt: 12,
+          mt: pathname === `/` || isMobile ? 12 : 16,
           minHeight: `100vh`,
           mb: 32,
         }}
