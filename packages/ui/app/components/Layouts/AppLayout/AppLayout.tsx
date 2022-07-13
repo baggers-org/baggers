f04c7543b78527ae00a7de710b42f1eae74b5e5e
Daniel Cooke <danielcooke1996@gmail.com>
2022-07-13 13:09:19 +0200

    Try removing is mobile wtf no errors

--- packages/ui/app/components/Layouts/AppLayout/AppLayout.tsx
+++ packages/ui/app/components/Layouts/AppLayout/AppLayout.tsx
@@ -1,20 +1,14 @@
 import { Box } from '@mui/system';
-import { useLocation } from '@remix-run/react';
-import React from 'react';
-
 import { AppBar } from '~/components/AppBar';
 import { Footer } from '~/components/Footer';
-import { useIsMobile } from '~/hooks';
 
 export const AppLayout: React.FC = ({ children }) => {
-  const { pathname } = useLocation();
-  const isMobile = useIsMobile();
   return (
     <>
       <AppBar />
       <Box
         sx={{
-          mt: pathname === `/` || isMobile ? 12 : 16,
+          mt: 12,
           minHeight: `100vh`,
           mb: 32,
         }}
