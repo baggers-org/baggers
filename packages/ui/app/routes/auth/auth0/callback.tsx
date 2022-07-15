566e061fffa32cae1d7f61785660ba5a61614d04
Daniel Cooke <danielcooke1996@gmail.com>
2022-07-15 16:22:27 +0200

    Okay i think it may be fixed

--- packages/ui/app/routes/auth/auth0/callback.tsx
+++ packages/ui/app/routes/auth/auth0/callback.tsx
@@ -3,14 +3,10 @@ import { baggersApiAuthenticator } from '~/auth.server';
 
 export const loader: LoaderFunction = async ({ request }) => {
   try {
-    console.log(`In callback`);
-
-    const res = await baggersApiAuthenticator.authenticate(`auth0`, request, {
+    return baggersApiAuthenticator.authenticate(`auth0`, request, {
       successRedirect: `/portfolios/created`,
       throwOnError: true,
     });
-
-    console.log(`Callback response `, res);
   } catch (e) {
     console.error(e);
 
