3cfcd3a12544e79409023a50cd994ec6776cfbec
Daniel Cooke <danielcooke1996@gmail.com>
2022-11-10 19:03:49 +0000

    Stop .env file failrue

--- libs/mongo-client/src/lib/env.ts
+++ libs/mongo-client/src/lib/env.ts
@@ -9,4 +9,10 @@ type Schema = Static<typeof schema>;
 
 export const env = envSchema<Schema>({
   schema,
+  data:
+    process.env.NODE_ENV === 'test'
+      ? {
+          POLYGON_API_KEY: 'test',
+        }
+      : undefined,
 });
--- libs/mongo-client/src/lib/mongo-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { mongoClient } from './mongo-client';
-
-describe('mongoClient', () => {
-  it('should work', () => {
-    expect(mongoClient()).toEqual('mongo-client');
-  });
-});
